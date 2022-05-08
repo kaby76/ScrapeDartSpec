ef37b9c220b74b1b5289e6bd7f49dd07b1738f59
Ken Domino <kenneth.domino@domemtech.com>
2022-05-08 07:53:30 -0400

    Updates.

--- refactor.sh
+++ refactor.sh
@@ -1,6 +1,11 @@
 #
 
 rm -rf xxx
-trparse -t antlr4 spec-grammar-2-15-dev.g4 | trsplit | trsponge -o xxx
-
+rm -f temp
+rm -rf tex-scraper/bin tex-scraper/obj
+cd tex-scraper
+dotnet build
+cd ..
+tex-scraper/bin/Debug/net6/ScrapeDartSpec.exe -file specs/spec-grammar-2-15-dev.tex > temp
+trparse -t antlr4 temp | trsplit | trsponge -o xxx
 
--- tex-scraper/Program.cs
+++ tex-scraper/Program.cs
@@ -15,7 +15,7 @@
         {
             for (int i = 0; i < args.Length; ++i)
             {
-                if (args[i].StartsWith("-scrape"))
+                if (args[i].StartsWith("-file"))
                 {
                     var fn = args[++i];
                     var input = System.IO.File.ReadAllText(fn);
@@ -25,46 +25,7 @@
                     display_tree = true;
                 else if (args[i].StartsWith("-tokens"))
                     display_tokens = true;
-                else
-                {
-                    var fn = args[i];
-                    var input = System.IO.File.ReadAllText(fn);
-                    ParseDart2(input);
-                }
             }
-        }
-
-        private static void ParseDart2(string input)
-        {
-            var str = new AntlrInputStream(input);
-            //System.Console.WriteLine(input);
-            var lexer = new NewDart2Lexer(str);
-            var tokens = new CommonTokenStream(lexer);
-            var parser = new NewDart2Parser(tokens);
-            var listener_lexer = new ErrorListener<int>();
-            var listener_parser = new ErrorListener<IToken>();
-            lexer.AddErrorListener(listener_lexer);
-            parser.AddErrorListener(listener_parser);
-            var tree = parser.compilationUnit();
-            if (display_tokens)
-            {
-                foreach (var t in tokens.GetTokens())
-                {
-                    System.Console.WriteLine(t.ToString());
-                }
-            }
-            if (display_tree)
-            {
-                System.Console.WriteLine(TreeOutput.OutputTree(tree, lexer, tokens).ToString());
-            }
-            if (listener_lexer.had_error || listener_parser.had_error)
-            {
-                System.Console.WriteLine("error in parse.");
-                throw new Exception();
-            }
-            else
-                System.Console.WriteLine("parse completed.");
-
         }
 
         static void Scrape(string input)
@@ -87,8 +48,7 @@
                 System.Console.WriteLine("error in parse.");
                 throw new Exception();
             }
-            else
-                System.Console.WriteLine("parse completed.");
+	    System.Console.WriteLine("grammar Temp;");
             var walker = new ParseTreeWalker();
             var listener = new Listen();
             walker.Walk(listener, tree);
